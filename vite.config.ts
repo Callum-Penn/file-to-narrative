f9a5b71a3ee8bcf2650a5dfc3bab4d4fe2719b34
Callum-Penn <callum@penn.co.uk>
2026-04-24 11:57:34 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // important for custom subdomain hosting
+  base: "/", // important for custom subdomain hosting 
   server: {
     host: "::",
     port: 8080,
